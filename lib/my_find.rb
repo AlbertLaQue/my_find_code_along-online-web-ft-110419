6a52cd1fbb6fcc6329cbafd9dd97a1377f0236d2
Albert LaQue <albertlaque@gmail.com>
2019-12-03 21:47:39 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,10 @@
 require 'pry'
 
 def my_find(collection)
-  i = 0 
-  while i < collection.length 
-  if  yield(collection[1])
-    return collection[i]
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
   end
-  i += 1 
- end 
 end
 
